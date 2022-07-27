ff3a0ddaa2128ad55960a9ab0c119160d1e94f0b
JuanC <camiloflstudio@gmail.com>
2022-07-26 20:53:28 -0500

    Levantando API fake

--- main.js
+++ main.js
@@ -1,119 +1,137 @@
 /* **********     Curso JavaScript: 119. APIs REST: CRUD con Axios (1/2) - #jonmircha     ********** */
-    /* **********     Curso JavaScript: 120. APIs REST: CRUD con Axios (2/2) - #jonmircha     ********** */
-    const d = document,
+/* **********     Curso JavaScript: 120. APIs REST: CRUD con Axios (2/2) - #jonmircha     ********** */
+const d = document,
+  $table = d.querySelector(".crud-table"),
+  $form = d.querySelector(".crud-form"),
+  $title = d.querySelector(".crud-title"),
+  $template = d.getElementById("crud-template").content,
+  $fragment = d.createDocumentFragment();
+const getAll = async () => {
+  try {
+    debugger;
+    let res = await axios.get("http://localhost:5555/santos"),
+      json = await res.data;
 
-      $table = d.querySelector(".crud-table"),
-      $form = d.querySelector(".crud-form"),
-      $title = d.querySelector(".crud-title"),
-      $template = d.getElementById("crud-template").content,
-      $fragment = d.createDocumentFragment();
+    console.log(json);
 
-    const getAll = async () => {
+    json.forEach((el) => {
+      $template.querySelector(".name").textContent = el.nombre;
+      $template.querySelector(".constellation").textContent = el.constelacion;
+      $template.querySelector(".edit").dataset.id = el.id;
+      $template.querySelector(".edit").dataset.name = el.nombre;
+      $template.querySelector(".edit").dataset.constellation = el.constelacion;
+      $template.querySelector(".delete").dataset.id = el.id;
+
+      let $clone = d.importNode($template, true);
+      $fragment.appendChild($clone);
+    });
+
+    $table.querySelector("tbody").appendChild($fragment);
+  } catch (err) {
+    let message = err.statusText || "Ocurrió un error";
+    $table.insertAdjacentHTML(
+      "afterend",
+      `<p><b>Error ${err.status}: ${message}</b></p>`
+    );
+  }
+};
+
+getAll();
+
+d.addEventListener("DOMContentLoaded", getAll);
+
+d.addEventListener("submit", async (e) => {
+  if (e.target === $form) {
+    e.preventDefault();
+
+    if (!e.target.id.value) {
+      //Create - POST
       try {
-        let res = await axios.get("http://localhost:5555/santos"),
+        let options = {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json; charset=utf-8",
+            },
+            data: JSON.stringify({
+              nombre: e.target.nombre.value,
+              constelacion: e.target.constelacion.value,
+            }),
+          },
+          res = await axios("http://localhost:5555/santos", options),
           json = await res.data;
 
-        console.log(json);
-
-        json.forEach(el => {
-          $template.querySelector(".name").textContent = el.nombre;
-          $template.querySelector(".constellation").textContent = el.constelacion;
-          $template.querySelector(".edit").dataset.id = el.id;
-          $template.querySelector(".edit").dataset.name = el.nombre;
-          $template.querySelector(".edit").dataset.constellation = el.constelacion;
-          $template.querySelector(".delete").dataset.id = el.id;
-
-          let $clone = d.importNode($template, true);
-          $fragment.appendChild($clone);
-        });
-
-        $table.querySelector("tbody").appendChild($fragment);
+        location.reload();
       } catch (err) {
         let message = err.statusText || "Ocurrió un error";
-        $table.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
+        $form.insertAdjacentHTML(
+          "afterend",
+          `<p><b>Error ${err.status}: ${message}</b></p>`
+        );
+      }
+    } else {
+      //Update - PUT
+      try {
+        let options = {
+            method: "PUT",
+            headers: {
+              "Content-type": "application/json; charset=utf-8",
+            },
+            data: JSON.stringify({
+              nombre: e.target.nombre.value,
+              constelacion: e.target.constelacion.value,
+            }),
+          },
+          res = await axios(
+            `http://localhost:5555/santos/${e.target.id.value}`,
+            options
+          ),
+          json = await res.data;
+
+        location.reload();
+      } catch (err) {
+        let message = err.statusText || "Ocurrió un error";
+        $form.insertAdjacentHTML(
+          "afterend",
+          `<p><b>Error ${err.status}: ${message}</b></p>`
+        );
       }
     }
+  }
+});
 
-    d.addEventListener("DOMContentLoaded", getAll);
+d.addEventListener("click", async (e) => {
+  if (e.target.matches(".edit")) {
+    $title.textContent = "Editar Santo";
+    $form.nombre.value = e.target.dataset.name;
+    $form.constelacion.value = e.target.dataset.constellation;
+    $form.id.value = e.target.dataset.id;
+  }
 
-    d.addEventListener("submit", async e => {
-      if (e.target === $form) {
-        e.preventDefault();
+  if (e.target.matches(".delete")) {
+    let isDelete = confirm(
+      `¿Estás seguro de eliminar el id ${e.target.dataset.id}?`
+    );
 
-        if (!e.target.id.value) {
-          //Create - POST
-          try {
-            let options = {
-              method: "POST",
-              headers: {
-                "Content-type": "application/json; charset=utf-8"
-              },
-              data: JSON.stringify({
-                nombre: e.target.nombre.value,
-                constelacion: e.target.constelacion.value
-              })
+    if (isDelete) {
+      //Delete - DELETE
+      try {
+        let options = {
+            method: "DELETE",
+            headers: {
+              "Content-type": "application/json; charset=utf-8",
             },
-              res = await axios("http://localhost:5555/santos", options),
-              json = await res.data;
+          },
+          res = await axios(
+            `http://localhost:5555/santos/${e.target.dataset.id}`,
+            options
+          ),
+          json = await res.data;
 
-            location.reload();
-          } catch (err) {
-            let message = err.statusText || "Ocurrió un error";
-            $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
-          }
-        } else {
-          //Update - PUT
-          try {
-            let options = {
-              method: "PUT",
-              headers: {
-                "Content-type": "application/json; charset=utf-8"
-              },
-              data: JSON.stringify({
-                nombre: e.target.nombre.value,
-                constelacion: e.target.constelacion.value
-              })
-            },
-              res = await axios(`http://localhost:5555/santos/${e.target.id.value}`, options),
-              json = await res.data;
-
-            location.reload();
-          } catch (err) {
-            let message = err.statusText || "Ocurrió un error";
-            $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
-          }
-        }
+        location.reload();
+      } catch (err) {
+        let message = err.statusText || "Ocurrió un error";
+        alert(`Error ${err.status}: ${message}`);
       }
-    });
-
-    d.addEventListener("click", async e => {
-      if (e.target.matches(".edit")) {
-        $title.textContent = "Editar Santo";
-        $form.nombre.value = e.target.dataset.name;
-        $form.constelacion.value = e.target.dataset.constellation;
-        $form.id.value = e.target.dataset.id;
-      }
-
-      if (e.target.matches(".delete")) {
-        let isDelete = confirm(`¿Estás seguro de eliminar el id ${e.target.dataset.id}?`);
-
-        if (isDelete) {
-          //Delete - DELETE
-          try {
-            let options = {
-              method: "DELETE",
-              headers: {
-                "Content-type": "application/json; charset=utf-8"
-              }
-            },
-              res = await axios(`http://localhost:5555/santos/${e.target.dataset.id}`, options),
-              json = await res.data;
-
-            location.reload();
-          } catch (err) {
-            let message = err.statusText || "Ocurrió un error";
-            alert(`Error ${err.status}: ${message}`);
-          }
-        }
-      }
-    });
+    }
+  }
+});
